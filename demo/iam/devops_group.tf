26f7fc33922e896262875284a17dd95c9882a3be
김보인 사원 Cloud Innovation Service팀 (charles1236@lgcns.com <charles1236@CNS.LGCNS.COM>
2023-08-25 14:31:35 +0900

    feat: add iam group membership

--- demo/iam/devops_group.tf
+++ demo/iam/devops_group.tf
@@ -1,3 +1,14 @@
 resource "aws_iam_group" "devops_group" {
     name = "devops"
 }
+
+resource "aws_iam_group_membership" "devops" {
+    name = aws_iam_group.devops_group.name
+
+    user = [
+        aws_iam_user.charles.name
+    ]
+
+    group = aws_iam_group.devops_group.name
+  
+}
