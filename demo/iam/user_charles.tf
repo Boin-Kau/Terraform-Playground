d093c621e5a66fe2f089cb90281bd297a20af272
김보인 사원 Cloud Innovation Service팀 (charles1236@lgcns.com <charles1236@CNS.LGCNS.COM>
2023-08-25 15:33:43 +0900

    feat: add admin policy to user charles

--- demo/iam/user_charles.tf
+++ demo/iam/user_charles.tf
@@ -1,3 +1,27 @@
 resource "aws_iam_user" "charles" {
     name = "charles" 
 } 
+
+resource "aws_iam_user_policy" "art_devops_black" {
+    name = "super-admin"
+    user = aws_iam_user.charles.name 
+
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement" : [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "*"
+            ],
+            "Resource" : [
+                "*"
+            ]
+        }
+    ]
+
+}
+EOF 
+  
+}
